44339f9c42037a3ab80d82ff1f1dbc4b619ebabf
uottoni <uottoni@gmail.com>
2015-02-04 17:44:51 -0200

    primeiro

---
+++ Quiron.LojaVirtual.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Quiron.LojaVirtual.Test
+{
+    [TestClass]
+    public class UnitTestQuiron
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
